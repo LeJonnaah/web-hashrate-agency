261fa229cde1d4317d5c61667b4b1feb04124d03
Jonathan Andueza <jonathanandueza1201@gmail.com>
2024-09-13 00:11:38 -0300

    carrousel integration & minor fixes

--- js/script.js
+++ js/script.js
@@ -7,3 +7,8 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+
+var flkty = new Flickity( '.main-gallery', {
+    cellAlign: 'left',
+    contain: true
+});
