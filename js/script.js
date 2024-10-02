8485dc38d134222b357754bf4d21b27999174462
Jonathan Andueza <jonathanandueza1201@gmail.com>
2024-10-01 23:28:57 -0300

    Jota fixes, iframe delete, footer links, form real

--- js/script.js
+++ js/script.js
@@ -39,3 +39,16 @@ const acc = document.getElementsByClassName("accordion");
 
 // Initialize a variable to store the index of the current accordion element
 let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        this.classList.toggle("active-accordion");
+        const panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
