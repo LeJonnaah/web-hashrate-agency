d36a2d7ce5ece18b253b73bf28fa6ca68cfaa2aa
Jonathan Andueza <jonathanandueza1201@gmail.com>
2024-10-02 22:39:14 -0300

    Styles fixes & lazy load

--- js/script.js
+++ js/script.js
@@ -52,3 +52,25 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+window.addEventListener('scroll', () => {
+    const elements = document.querySelectorAll('.lazy');
+    elements.forEach(element => {
+        if (isInViewport(element)) {
+            console.log(element);
+            // Carga el elemento
+            element.classList.remove('lazy');
+            element.classList.add('lazy-loaded');
+        }
+    });
+});
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
